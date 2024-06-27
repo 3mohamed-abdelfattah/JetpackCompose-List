25fff5421ffd499a853b6254a538c8402da8e211
MohamedMohamed <m01126165016@icloud.com>
2024-06-27 03:22:53 +0300

    make card

--- app/src/main/java/com/example/jetpackcompose_list/view/SimpleList.kt
+++ app/src/main/java/com/example/jetpackcompose_list/view/SimpleList.kt
@@ -1,21 +1,35 @@
 package com.example.jetpackcompose_list.view
 
 import android.content.Context
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.jetpackcompose_list.R
 import com.example.jetpackcompose_list.response.SimpleData
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -49,7 +63,36 @@ fun SimpleList(navController: NavController) {
                 fontWeight = FontWeight.Bold
             )
         }
+        LazyColumn(modifier = Modifier.padding(10.dp)) {
+            items(simpleData) { data -> SampleDataListItem(data, navController) }
+        }
+    }
+}
 
+@Composable
+fun SampleDataListItem(data: SimpleData, navController: NavController) {
+    Card(
+        modifier = Modifier
+            .clickable {
+                val itemVal = Gson().toJson(data)
+                navController.navigate("simple_detail/$itemVal")
+            }
+            .padding(10.dp)
+            .fillMaxSize(),
+//        elevation = 5.dp
+        shape = RoundedCornerShape(5.dp)
+    ) {
+        Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()) {
+            Image(
+                painterResource(R.drawable.baseline_cruelty_free_24),
+                contentDescription = "Photo", modifier = Modifier
+                    .width(60.dp)
+                    .height(60.dp)
+                    .clip(
+                        RoundedCornerShape(5.dp)
+                    )
+            )
+        }
     }
 }
 
